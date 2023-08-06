8e4a863160ef1aa04a11c7939f5fa4d01d2b1a46
HamzaDevs <hamza.devs@gmail.com>
2023-08-06 15:26:48 +0200

    setup backend with mongodb

---
+++ server/databases/conn.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+async function connect() {
+  const mongodb = await MongoMemoryServer.create();
+  const uri = mongodb.getUri();
+  mongoose.set('strictQuery', true)
+  const db = await mongoose.connect(uri);
+  console.log("Database connected");
+  return db;
+}
+
+export default connect;
